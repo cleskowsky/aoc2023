72f921ac22339a8dc5067338bbebad269ba5010c
Christian Leskowsky <christian@leskowsky.net>
2024-08-04 12:54:10 -0400

    Day 2 part 2

--- day2.py
+++ day2.py
@@ -1,4 +1,7 @@
 import re
+from collections import defaultdict
+from functools import reduce
+from operator import mul
 
 s = 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green'
 print(s)
@@ -34,10 +37,28 @@ def possible_game(game, bag):
     return all(x[0] <= bag[x[1]] for x in game['cubes'])
 
 
-assert possible_game(g, bag)
+ assert possible_game(g, bag)
 
 g = parse_game('Game 1: 13 red')
 assert not possible_game(g, bag)
 
 valid_games = filter(lambda x: possible_game(x, bag), map(parse_game, open('day2.txt').readlines()))
 assert 1734 == sum(x['id'] for x in valid_games)
+
+# Part 2
+
+games = map(parse_game, open('day2.txt').readlines())
+
+sum = 0
+for game in games:
+    cubes_needed = defaultdict(int)
+    for cube in game['cubes']:
+        colour = cube[1]
+        x = cube[0]
+        if x > cubes_needed[colour]:
+            cubes_needed[colour] = x
+
+    sum += reduce(mul, cubes_needed.values())
+
+print(sum)
+assert 70387 == sum
